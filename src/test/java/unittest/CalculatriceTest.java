d4de7b3ed830f755702389dc4abe77b9f3fc8739
Saijitsu <zeromus16@msn.com>
2019-05-29 15:47:09 +0200

    Add console message

--- src/test/java/unittest/CalculatriceTest.java
+++ src/test/java/unittest/CalculatriceTest.java
@@ -11,15 +11,26 @@ public class CalculatriceTest {
 		// fail("Not yet implemented");
 		Assert.assertEquals(3, Calculatrice.divise(6, 2));
 	}
+
 	@Test
 	public void diviseTest3() throws MonException {
-		// fail("Not yet implemented");
 		Assert.assertEquals(5, Calculatrice.divise(10, 2));
 	}
-	@Test(expected=MonException.class)
+
+	@Test(expected = MonException.class)
 	public void diviseTest2() throws MonException {
-		// fail("Not yet implemented");
 		Assert.assertEquals(3, Calculatrice.divise(6, 0));
+	}
+	
+	@Test
+	public void diviseTest4() throws MonException {
+		Assert.assertEquals(3, Calculatrice.divise(6, 2));
+		try {
+			Calculatrice.divise(6, 0);
+		} catch (Exception e) {
+			Assert.assertEquals("Ca ne marche pas!", e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 }
